bad46c1a6ab00de7d1ae5c7a9d5a5f5b8bf9da62
Jazper <jazpersaldana@yahoo.com>
2024-05-25 15:14:01 -0500

    badges and schedules lab complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = [f"Hello, my name is {name}." for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    new_list = [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
+    return new_list
 
 def printer(names):
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for room in assign_rooms(names):
+        print(room)
     return None
